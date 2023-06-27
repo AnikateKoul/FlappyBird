a154e83714e5449776105977e5af81719efc114d
AnikateKoul <lcs2021012@iiitl.ac.in>
2023-06-27 17:06:15 +0530

    Crude UI and logic done

--- 2D Game/Assets/Scripts/BirdScript.cs
+++ 2D Game/Assets/Scripts/BirdScript.cs
@@ -6,18 +6,26 @@ public class BirdScript : MonoBehaviour
 {
     public Rigidbody2D rigidBody;
     public float flapStrength = 10;
+    public LogicScript logic;
+    bool isBirdAlive = true;
     // Start is called before the first frame update
     void Start()
     {
-        
+        logic = GameObject.FindGameObjectWithTag("Logic").GetComponent<LogicScript>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space))
+        if(isBirdAlive && Input.GetKeyDown(KeyCode.Space))
         {
             rigidBody.velocity = Vector2.up * flapStrength;
         }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        isBirdAlive = false;
+        logic.gameOver();
     }
 }
---
+++ 2D Game/Assets/Scripts/LogicScript.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LogicScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int playerScore;
+    public Text scoreText;
+    public GameObject gameOverScreen;
+
+    [ContextMenu("Add Score")]
+    public void addScore()
+    {
+        playerScore++;
+        scoreText.text = playerScore.ToString();
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void gameOver()
+    {
+        gameOverScreen.SetActive(true);
+    }
+}
