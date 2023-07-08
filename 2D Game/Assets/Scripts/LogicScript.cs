d5e99671c1454bc0d8e6101a262719f4c61abf54
AnikateKoul <lcs2021012@iiitl.ac.in>
2023-07-08 13:11:28 +0530

    Added high score functionality

--- 2D Game/Assets/Scripts/LogicScript.cs
+++ 2D Game/Assets/Scripts/LogicScript.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using System;
 
 public class LogicScript : MonoBehaviour
 {
@@ -11,6 +12,8 @@ public class LogicScript : MonoBehaviour
     public Text scoreText;
     public GameObject gameOverScreen;
     public BirdScript birdScript;
+    public Text gameScore;
+    public Text gameHighScore;
 
     private void Start()
     {
@@ -32,16 +35,34 @@ public class LogicScript : MonoBehaviour
         {
             playerScore += score;
             scoreText.text = playerScore.ToString();
+            gameScore.text = $"Score : {playerScore}";
         }
     }
 
     public void RestartGame()
     {
+        Console.Write("Game restarted");
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 
     public void gameOver()
     {
+        if(PlayerPrefs.HasKey("HighScore"))
+        {
+            int temp = PlayerPrefs.GetInt("HighScore");
+            if (temp < playerScore)
+            {
+                temp = playerScore;
+            }
+            PlayerPrefs.SetInt("HighScore", temp);
+        }
+        else
+        {
+            PlayerPrefs.SetInt("HighScore", playerScore);
+        }
+        int highScore = PlayerPrefs.GetInt("HighScore");
+        PlayerPrefs.Save();
+        gameHighScore.text = $"High Score : {highScore}";
         gameOverScreen.SetActive(true);
     }
 }
